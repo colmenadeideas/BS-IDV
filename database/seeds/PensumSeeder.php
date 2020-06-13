6545755005bcdb71dcb301be0663a878a34a64bd
aortega90 <aortega@besign.com.ve>
2020-06-12 22:06:16 -0400

    corrigiendo algunos archivos de migracion

--- database/seeds/PensumSeeder.php
+++ database/seeds/PensumSeeder.php
@@ -13,12 +13,13 @@ class PensumSeeder extends Seeder
     {
          $carrera =     ["Diseño Grafico", "Diseño de Modas", "Ilustración","Diseño Industrial", "Diseño de interiores"];
          foreach ($carrera as $key ) {
-         	$id = DB::table('pensum')->insertGetId(
-                ['name' => $name, 
-                'email' => $email,
-                'email_verified_at' => now(),
-                'password' => '$2y$10$v0gb8RrOMWIwziF80vsV5.fWaHCVAjOJfDethguZKNL8SY7VxNs5m',
-                'remember_token' => Str::random(10)]);
+         	DB::table('pensum')->insert(
+                ['start_date' => now(), 
+                 'end_date' => now(), 
+                 'status' => 'active',
+                 'matters' => 'materias',
+                'caree' => $key,
+                'description' => $key]);
          }
     }
 }
--- database/seeds/PermissionsDemoSeeder.php
+++ database/seeds/PermissionsDemoSeeder.php
@@ -18,29 +18,32 @@ class PermissionsDemoSeeder extends Seeder
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
         // create permissions
-        Permission::create(['name' => 'edit schudele']);
-        Permission::create(['name' => 'delete schudele']);
-        Permission::create(['name' => 'publish schudele']);
-        Permission::create(['name' => 'unpublish schudele']);
-        Permission::create(['name' => 'show schudele']);
+        Permission::create(['name' => 'edit matter']);
+        Permission::create(['name' => 'delete matter']);
+        Permission::create(['name' => 'publish matter']);
+        Permission::create(['name' => 'unpublish matter']);
+        Permission::create(['name' => 'show matter']);
+        Permission::create(['name' => 'notes matter']);
 
         // create roles and assign existing permissions
         $role1 = Role::create(['name' => 'admin']);
-        $role1->givePermissionTo('edit schudele');
-        $role1->givePermissionTo('delete schudele');
-        $role1->givePermissionTo('publish schudele');
-        $role1->givePermissionTo('unpublish schudele');
-        $role1->givePermissionTo('show schudele');
+        $role1->givePermissionTo('edit matter');
+        $role1->givePermissionTo('delete matter');
+        $role1->givePermissionTo('publish matter');
+        $role1->givePermissionTo('unpublish matter');
+        $role1->givePermissionTo('show matter');
+        $role1->givePermissionTo('notes matter');
 
 
         $role2 = Role::create(['name' => 'coordinator']);
-        $role2->givePermissionTo('show schudele');
+        $role2->givePermissionTo('show matter');
 
         $role3 = Role::create(['name' => 'teacher']);
-        $role3->givePermissionTo('show schudele');
+        $role3->givePermissionTo('show matter');
+        $role3->givePermissionTo('notes matter');
 
         $role4 = Role::create(['name' => 'student']);
-        $role4->givePermissionTo('show schudele');
+        $role4->givePermissionTo('show matter');
 
         // gets all permissions via Gate::before rule; see AuthServiceProvider
 
